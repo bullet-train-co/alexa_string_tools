9821e6bccb278f48a004c371f224e09f8b9a9852
Andrew Culver <andrew.culver@gmail.com>
2018-05-28 09:26:15 -0700

    Update alexa_string_tools.rb

--- lib/alexa_string_tools.rb
+++ lib/alexa_string_tools.rb
@@ -2,35 +2,35 @@ require "alexa_string_tools/version"
 require "humanize"
 
 module AlexaStringTools
-  def body_as_email
-    string = spoken_string_to_email(self)
+  def email_from_alexa
+    string = convert_from_alexa_string_to_email(self)
     string.strip
   end
 
   private
 
-  def spoken_string_to_email(string)
+  def convert_from_alexa_string_to_email(string)
 
-    spoken_string_mapping.each do |from,to|
+    alexa_string_mapping.each do |from,to|
       string = string.gsub(from, to)
     end
 
     string.gsub(' ', '')
   end
 
-  def spoken_string_mapping
-    return if @spoken_string_mapping
+  def self.alexa_string_mapping
+    return if @alexa_string_mapping
 
-    @spoken_string_mapping = {}
+    @alexa_string_mapping = {}
 
     # we don’t have to pad these with spaces because the results are crunched together in the end.
     ('A'..'Z').to_a.each do |letter|
-      @spoken_string_mapping["#{letter}."] = letter
-      @spoken_string_mapping["#{letter.downcase}."] = letter.downcase
+      @alexa_string_mapping["#{letter}."] = letter
+      @alexa_string_mapping["#{letter.downcase}."] = letter.downcase
     end
 
     # it’s okay to pad these with spaces, because they can’t be at the beginning or end anyway.
-    @spoken_string_mapping.merge!({
+    @alexa_string_mapping.merge!({
       ' at ' => '@',
       ' dot ' => '.',
       ' period ' => '.',
@@ -43,10 +43,10 @@ module AlexaStringTools
       number_as_string = number.humanize
       number_as_string = number_as_string.gsub(/,/, '')
       number_as_string = number_as_string.gsub(/-/, ' ')
-      @spoken_string_mapping[number_as_string] = number.to_s
+      @alexa_string_mapping[number_as_string] = number.to_s
     end
 
-    @spoken_string_mapping
+    @alexa_string_mapping
   end
 end
 
