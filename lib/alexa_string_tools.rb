83c07f23909272993531f99377f3f3dff8f16c9f
Andrew Culver <andrew.culver@gmail.com>
2018-05-28 10:43:07 -0700

    Generate the string mapping table once at load time.

--- lib/alexa_string_tools.rb
+++ lib/alexa_string_tools.rb
@@ -2,6 +2,10 @@ require "alexa_string_tools/version"
 require "humanize"
 
 module AlexaStringTools
+
+  # we'll lazy load this mapping.
+  @@alexa_string_mapping = nil
+
   def email_from_alexa
     string = convert_from_alexa_string_to_email(self)
     string.strip
@@ -19,18 +23,18 @@ module AlexaStringTools
   end
 
   def alexa_string_mapping
-    return if @alexa_string_mapping
+    return @@alexa_string_mapping if @@alexa_string_mapping
 
-    @alexa_string_mapping = {}
+    @@alexa_string_mapping = {}
 
     # we don’t have to pad these with spaces because the results are crunched together in the end.
     ('A'..'Z').to_a.each do |letter|
-      @alexa_string_mapping["#{letter}."] = letter
-      @alexa_string_mapping["#{letter.downcase}."] = letter.downcase
+      @@alexa_string_mapping["#{letter}."] = letter
+      @@alexa_string_mapping["#{letter.downcase}."] = letter.downcase
     end
 
     # it’s okay to pad these with spaces, because they can’t be at the beginning or end anyway.
-    @alexa_string_mapping.merge!({
+    @@alexa_string_mapping.merge!({
       ' at ' => '@',
       ' dot ' => '.',
       ' period ' => '.',
@@ -43,13 +47,16 @@ module AlexaStringTools
       number_as_string = number.humanize
       number_as_string = number_as_string.gsub(/,/, '')
       number_as_string = number_as_string.gsub(/-/, ' ')
-      @alexa_string_mapping[number_as_string] = number.to_s
+      @@alexa_string_mapping[number_as_string] = number.to_s
     end
 
-    @alexa_string_mapping
+    @@alexa_string_mapping
   end
 end
 
 String.class_eval do
   include AlexaStringTools
 end
+
+# trigger the loading of the string mapping automatically at load time.
+"".email_from_alexa
