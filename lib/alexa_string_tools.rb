3a5c2ca83d1fe84815ca891ab41b020c09891182
Dmitry Gorodnichy <gorodni4ij@gmail.com>
2018-05-29 15:22:05 +0300

    Generate the string replacement table during gem init.

--- lib/alexa_string_tools.rb
+++ lib/alexa_string_tools.rb
@@ -3,60 +3,70 @@ require "humanize"
 
 module AlexaStringTools
 
-  # we'll lazy load this mapping.
-  @@alexa_string_mapping = nil
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :mapping, :alexa_string_mapping
+
+    def initialize
+      @alexa_string_mapping = alexa_string_mapping
+    end
+
+    def alexa_string_mapping
+      mapping = {}
+
+      # we don’t have to pad these with spaces because the results are crunched together in the end.
+      ('A'..'Z').to_a.each do |letter|
+        mapping["#{letter}."] = letter
+        mapping["#{letter.downcase}."] = letter.downcase
+      end
+
+      # spoken numbers are always converted into integers.
+      (0..10000).to_a.reverse.each do |number|
+        number_as_string = number.humanize
+        number_as_string = number_as_string.gsub(/,/, '')
+        number_as_string = number_as_string.gsub(/-/, ' ')
+        mapping[number_as_string] = number.to_s
+      end
+
+      mapping
+    end
+  end
 
   def email_from_alexa
+    raise_if_not_configured
     string = convert_from_alexa_string_to_email(self)
     string.strip
   end
 
   private
 
-  def convert_from_alexa_string_to_email(string)
+  def raise_if_not_configured
+    if AlexaStringTools.configuration.nil?
+      raise StandardError.new(
+        "alexa_string_tools gem is't configured. \nRun rails g alexa_string_tools:install to fix it."
+      )
+    end
+  end
 
-    alexa_string_mapping.each do |from,to|
+  def convert_from_alexa_string_to_email(string)
+    mapping = AlexaStringTools.configuration.alexa_string_mapping.merge(AlexaStringTools.configuration.mapping)
+    mapping.each do |from,to|
       string = string.gsub(from, to)
     end
 
     string.gsub(' ', '')
   end
 
-  def alexa_string_mapping
-    return @@alexa_string_mapping if @@alexa_string_mapping
-
-    @@alexa_string_mapping = {}
-
-    # we don’t have to pad these with spaces because the results are crunched together in the end.
-    ('A'..'Z').to_a.each do |letter|
-      @@alexa_string_mapping["#{letter}."] = letter
-      @@alexa_string_mapping["#{letter.downcase}."] = letter.downcase
-    end
-
-    # it’s okay to pad these with spaces, because they can’t be at the beginning or end anyway.
-    @@alexa_string_mapping.merge!({
-      ' at ' => '@',
-      ' dot ' => '.',
-      ' period ' => '.',
-      ' underscore ' => '_',
-      ' plus ' => '+',
-    })
-
-    # spoken numbers are always converted into integers.
-    (0..10000).to_a.reverse.each do |number|
-      number_as_string = number.humanize
-      number_as_string = number_as_string.gsub(/,/, '')
-      number_as_string = number_as_string.gsub(/-/, ' ')
-      @@alexa_string_mapping[number_as_string] = number.to_s
-    end
-
-    @@alexa_string_mapping
-  end
 end
 
 String.class_eval do
   include AlexaStringTools
 end
-
-# trigger the loading of the string mapping automatically at load time.
-"".email_from_alexa
---
+++ lib/generators/templates/alexa_string_tools_initializer.rb
@@ -0,0 +1,9 @@
+AlexaStringTools.configure do |config|
+  config.mapping = {
+    ' at ' => '@',
+    ' dot ' => '.',
+    ' period ' => '.',
+    ' underscore ' => '_',
+    ' plus ' => '+'
+  }
+end
